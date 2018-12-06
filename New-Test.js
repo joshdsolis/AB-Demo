52ad117a0166b2245f5306a19573d7b05caa2701
joshdsolis <39255273+joshdsolis@users.noreply.github.com>
2018-12-06 15:16:25 -0600

    Update New-Test.js

--- New-Test.js
+++ New-Test.js
@@ -1,4 +1,6 @@
- <script type="text/javascript">
+# New 
+
+<script type="text/javascript">
      ABalytics.init({
          experiment1: [
              {
@@ -23,3 +25,21 @@
          ABalytics.applyHtml();
      };
     </script>
+
+# Old
+ABalytics.init({
+         experiment1: [
+             {
+                 name: 'variant1',
+                 'experiment1_text': 'Pretty please click the button with sugar on top!',
+                 'experiment1_button':
+                       '<input type="button" value="Click me!" onclick="ga(\'send\', \'event\', \'buttonclick\', \'nice\'); alert(\'Thanks!\');">'
+             },
+             {
+                 name: 'variant2',
+                 'experiment1_text': 'You better click that button...',
+                 'experiment1_button':
+                       '<input type="button" value="Click me, OR ELSE" onclick="ga(\'send\', \'event\', \'buttonclick\', \'stern\'); alert(\'You will be spared.\');">'
+             }
+         ]
+     });
